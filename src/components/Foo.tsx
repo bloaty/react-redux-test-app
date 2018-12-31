2b641b64a4b19e98e80e22cb2d440ede786e4bef
bloaty <bloaty@bloaty.solutions>
2018-12-30 23:30:14 -0800

    try to use CSS modules why not

--- src/components/Foo.tsx
+++ src/components/Foo.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 
+import * as styles from "styles/Foo.module.scss";
+
 export interface FooProps {
     header: string;
     body: string;
 }
 
-export const Foo: React.FunctionComponent<FooProps> = (props) => {
-    return (
-        <>
-            <h1>{props.header}</h1>
-            <p>{props.body}</p>
-        </>
-    );
-};
+export const Foo: React.FunctionComponent<FooProps> = (props) => (
+    <div className={styles.h1}>
+        <h1>{props.header}</h1>
+        <p>{props.body}</p>
+    </div>
+);
